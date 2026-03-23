099b94538b1237c1146a1332fdc20530266d7e3c
seoseonglyeol <seoseonglyeol.research@gmail.com>
2026-03-23 14:39:52 +0900

    password

--- kong.py
+++ kong.py
@@ -1,3 +1,17 @@
+'''
 print( "hello")
 kongsuni= 'concong'
 print( kongsuni)
+'''
+'''
+password= int(input("enter the password: "))
+if password == "Jye":
+    print("correct")
+else:
+    print("incorrect")
+'''
+password= int(input("enter the password: "))
+if password == 1234:
+    print("correct")
+else:
+    print("incorrect")
